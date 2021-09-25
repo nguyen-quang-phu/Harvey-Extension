b36811d73227ddaa244808eef0ee09ed68dd3455
Otis <otis.nguyen.goldenowl@gmail.com>
2021-09-25 20:54:45 +0700

    update keep old style sheet

--- src/extension.ts
+++ src/extension.ts
@@ -1,38 +1,51 @@
 import * as vscode from 'vscode';
-import { createStyleSheet } from './utils';
+import { createStyleSheet, getLineEndStyleSheet } from './utils';
 
 export function activate(context: vscode.ExtensionContext) {
   console.log('Congratulations, your extension "helloworld" is now active!');
 
-  let disposable = vscode.commands.registerCommand('extension.createStyleSheet', () => {
-    let editor = vscode.window.activeTextEditor;
-    if (!editor) {
-      vscode.window.showInformationMessage('Create Stylesheet Fail');
-      return;
-    }
-    const document = editor.document;
-    const fullText = document.getText();
-    const listStyles = fullText.match(/styles.([a-zA-Z]+)/g)?.map((nameClass) => {
-      return nameClass.split('.')[1];
-    });
-    const lines = fullText.split('\n');
-    let lineContainStyleSheet = 0;
-    lines.forEach((line, index) => {
-      if (line.match(/const styles = StyleSheet.create/)) {
-        lineContainStyleSheet = index;
+  let disposable = vscode.commands.registerCommand(
+    'extension.createStyleSheet',
+    () => {
+      let editor = vscode.window.activeTextEditor;
+      if (!editor) {
+        vscode.window.showInformationMessage('Create Stylesheet Fail');
+        return;
       }
-    });
-    editor.edit((editBuilder) => {
-      editBuilder.replace(
+      const document = editor.document;
+      const fullText = document.getText();
+      const listStyles = fullText
+        .match(/styles.([a-zA-Z]+)/g)
+        ?.map((nameClass) => {
+          return nameClass.split('.')[1];
+        });
+      const lines = fullText.split('\n');
+      let lineContainStyleSheet = 0;
+      lines.forEach((line, index) => {
+        if (line.match(/const styles = StyleSheet.create/)) {
+          lineContainStyleSheet = index;
+        }
+      });
+      const textContainStyleSheet = document.getText(
         new vscode.Range(
           new vscode.Position(lineContainStyleSheet, 0),
-          new vscode.Position(lineContainStyleSheet + 1, 0),
+          new vscode.Position(document.lineCount, 0),
         ),
-        createStyleSheet(listStyles),
       );
-    });
-    vscode.window.showInformationMessage('Create Stylesheet Success');
-  });
+      const end = getLineEndStyleSheet(textContainStyleSheet);
+
+      editor.edit((editBuilder) => {
+        editBuilder.replace(
+          new vscode.Range(
+            new vscode.Position(lineContainStyleSheet, 0),
+            new vscode.Position(lineContainStyleSheet + end + 1, 0),
+          ),
+          createStyleSheet(listStyles, textContainStyleSheet),
+        );
+      });
+      vscode.window.showInformationMessage('Create Stylesheet Success');
+    },
+  );
 
   context.subscriptions.push(disposable);
 }
--- src/utils.ts
+++ src/utils.ts
@@ -1,12 +1,58 @@
-export const createStyleSheet = (listStyles: string[] | undefined) => {
+export const extractStyleSheet = (text: string) => {
+  const end = text.indexOf(';');
+  return text.slice(0, end + 1);
+};
+
+interface ListStyles {
+  [key: string]: string;
+}
+
+export const extractListStyles = (text: string) => {
+  const list = text.split(/},\n/);
+  list[0] = list[0].split('\n').slice(1).join('\n');
+  const res = list.slice(0, list.length - 1).reduce((pre, style) => {
+    const temp = style.trim() + '\n\t},\n';
+    const index = temp.indexOf(':');
+    const key = temp.slice(0, index);
+    const value = temp.slice(index + 1).trim();
+    return { ...pre, [key]: value };
+  }, {} as ListStyles);
+  // console.log(res);
+  return res;
+};
+export const getLineEndStyleSheet = (textContainStyleSheet: string) => {
+  const list = textContainStyleSheet.split('\n');
+  const n = list.length;
+  let res = -1;
+  for (let i = 0; i < n; i++) {
+    if (list[i].includes(';')) {
+      res = i;
+      break;
+    }
+  }
+  return res;
+};
+
+export const createStyleSheet = (
+  listStyles: string[] | undefined,
+  textContainStyleSheet: string,
+) => {
   if (!listStyles) {
     return `const styles = StyleSheet.create({});`;
   }
+  const uniqueListStyles = Array.from(new Set(listStyles));
+  const currentStyleSheet = extractStyleSheet(textContainStyleSheet);
+  const oldListStyles = extractListStyles(currentStyleSheet);
   const separator = ':{\n\t\t\n\t},\n';
-  let styleSheet = listStyles.reduce((pre, curr) => {
+  let styleSheet = uniqueListStyles.reduce((pre, curr) => {
+    const properties = oldListStyles[curr];
+    if (properties) {
+      return pre + '\t' + curr + ':' + properties + '\n';
+    }
     return pre + '\t' + curr + separator;
   }, '');
   styleSheet = styleSheet.slice(0, styleSheet.length - 2);
+  console.log(styleSheet);
   return `const styles = StyleSheet.create({\n${styleSheet}\n});
 `;
 };
